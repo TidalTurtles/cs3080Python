caed4b661a671f53db58dc6b79fe8e54a5d86117
Noah Holt <tidalturtles@gmail.com>
2022-08-30 09:46:51 -0600

    made a test and began 1
    Made test for the new IDE and then began the first assignment by making comments as to what to do

---
+++ hw1_noah_holt.py
@@ -0,0 +1,17 @@
+# Homework_1
+# Noah_Holt
+# Due: 2 sept 2022
+# Security Program
+#   Ask the user a series of questions and see if they can answer them
+
+# Needed
+#   1) min 1 print(), input(), str(), len(), int() randint()
+#   2) one var for int, float and string
+#   3) pick camelcase or underscores
+#   4) use 4 math operations (one must be % or //
+#   5) use for loop to ask 3 question and use the range() (use i for the questions)
+#   6) need one each of if, elif, else, break, continue
+#   7) need one truthy or falsey
+#   8) Comment it good
+
+
