e81bfd3d7ff3aa3a99f61f2604d4f2c6b2406d58
Noah Holt <tidalturtles@gmail.com>
2022-09-20 09:06:36 -0600

    comment goals
    added comments with exactly what is needed for part 3 and so I just have that and the try catch for part 2. and I am done

---
+++ hw2_noah_holt/hw2_noah_holt_ex_3.py
@@ -0,0 +1,17 @@
+# Homework_2_3
+# Noah_Holt
+# Due: 23 sept 2022
+# List manipulation Program
+#
+# 1)  make list of strings, and print with one liner
+# 2)  sort() and print again
+# 3)  print only 1st item
+# 4)  using slicing, print all but first
+# 5)  print last with neg index
+# 6)  print Keys index with index()
+# 7)  append "Tablet", print list
+# 8)  insert "Mask as second item, print list
+# 9)  remove "Phone", print list
+# 10) reverse and print
+# 11) make function strList(), takes list as arg
+#       prints in special format for any list
