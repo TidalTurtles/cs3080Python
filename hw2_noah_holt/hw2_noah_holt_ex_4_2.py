a29822439ffcf3ac772f01ad3bd8564c52f062af
Noah Holt <tidalturtles@gmail.com>
2022-09-23 17:15:31 -0600

    part4 step 2
    all finished with part 4 step2, making semi random limits and then playing the same game as step 1

---
+++ hw2_noah_holt/hw2_noah_holt_ex_4_2.py
@@ -0,0 +1,33 @@
+# Noah_Holt
+# Due: 23 sept 2022
+# guess number program
+# 1) make random num between 1-20
+# 2) big while loop till user input is random number
+# 3) print that they are right at the number and guesses it took
+#
+# 2) More complicated
+#    Make the upper and lower bound random as well
+
+import random
+
+# start var
+lowerLimit = random.randint(0, 51)
+upperLimit = random.randint(51, 101)
+guessThis = random.randint(lowerLimit, upperLimit)
+userGuess = 0
+guessCount = 0
+print("I'm thinking of a number...")
+
+while userGuess != guessThis and guessCount < 10:
+    print("Which number is it (hint: between (" + str(lowerLimit) + "-" + str(upperLimit) + ") You have 10 guesses!")
+    guessCount += 1
+    userGuess = int(input())
+    if userGuess == guessThis:
+        print("You guessed right!!! The number was " + str(userGuess))
+    else:
+        print("Nope, not quite. How about you try again!")
+
+if guessCount < 10:
+    print("Nice job! it only took you " + str(guessCount) + " tries to guess!")
+else:
+    print("Sorry,  the number I was thinking of was " + str(guessThis))
