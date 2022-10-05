2578880684c80e66906ce48d95be72cb175edde8
Noah Holt <tidalturtles@gmail.com>
2022-10-05 11:39:04 -0600

    Homework 3 part 2
    Finished homework 3 part 2, took me a minute or two but found out I was overcomplicating it really bad but I found a simple solution in the book that was all I needed to do.

--- hw3_noah_holt/hw3_noah_holt_ex_2.py
+++ hw3_noah_holt/hw3_noah_holt_ex_2.py
@@ -7,3 +7,20 @@
 # 2) store in dictionary
 # 3) print with pprint (see example on assignment)
 
+import pprint
+
+# test case: The quick brown fox jumps over the lazy dog.
+# contains all letters
+
+testString = "My name is Noah Holt. I am vegan!"
+
+# initialize dictionary
+lettersCounted = {}
+
+# found similar in book chapter 5
+# in the setDefault section
+for character in testString:
+    lettersCounted.setdefault(character, 0)
+    lettersCounted[character] = lettersCounted[character] + 1
+
+pprint.pprint(lettersCounted)
