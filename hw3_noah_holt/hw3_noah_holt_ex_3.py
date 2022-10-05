08f7dcf2d390d84ee0084822c4988ee0d6332cd6
Noah Holt <tidalturtles@gmail.com>
2022-10-05 12:01:41 -0600

    homework 3 part 3
    started homework 3 part 3, did the first several parts. just have one or 2 left. class is over so gonna go get some lunch

--- hw3_noah_holt/hw3_noah_holt_ex_3.py
+++ hw3_noah_holt/hw3_noah_holt_ex_3.py
@@ -8,4 +8,29 @@
 # 3) make function to add items
 # 4) make function to delete
 
+def printInventory(inventory):
+    # borrowed from format.py in class as assignment says to do
+    print('INVENTORY ITEMS'.center(14 + 10, '-'))
+    for k, v in inventory.items():
+        print(k.ljust(14, '.') + str(v).rjust(10))
 
+def addItem(inventory, item):
+    inventory.setdefault(item, 0)
+    inventory[item] = inventory[item] + 1
+
+
+def deleteItem(inventory, item):
+    if inventory.keys(item) == 0:
+        inventory[item] = inventory[item] - 1
+
+
+inventoryItems = {'Hand sanitizer': 10, 'Soap': 6, 'Kleenex': 22, 'Lotion': 16, 'Razors': 12}
+# test 1
+printInventory(inventoryItems)
+# test 2
+addItem(inventoryItems, 'Advil')
+addItem(inventoryItems, 'Advil')
+addItem(inventoryItems, 'Advil')
+addItem(inventoryItems, 'Advil')
+printInventory(inventoryItems)
+# test 3
