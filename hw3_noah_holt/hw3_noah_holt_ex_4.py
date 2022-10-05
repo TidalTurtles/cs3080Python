0f43a770ea095f1957112b8cf9f9b3a67d4c287e
Noah Holt <tidalturtles@gmail.com>
2022-10-05 12:51:55 -0600

    All done
    all of homework 3 is done.

--- hw3_noah_holt/hw3_noah_holt_ex_4.py
+++ hw3_noah_holt/hw3_noah_holt_ex_4.py
@@ -7,4 +7,41 @@
 # 2) function to print board
 # 3) make code for player input
 
+# found help in book chapter 5
+def printBoard(board):
+    print(board['top-Left'] + ' | ' + board['top-Mid'] + ' | ' + board['top-Right'])
+    print('- + - + -')
+    print(board['mid-Left'] + ' | ' + board['mid-Mid'] + ' | ' + board['mid-Right'])
+    print('- + - + -')
+    print(board['low-Left'] + ' | ' + board['low-Mid'] + ' | ' + board['low-Right'])
 
+
+def placeChar(board, character):
+    place = input()
+    board[place] = character
+
+
+
+theBoard = {'top-Left': ' ', 'top-Mid': ' ', 'top-Right': ' ',
+            'mid-Left': ' ', 'mid-Mid': ' ', 'mid-Right': ' ',
+            'low-Left': ' ', 'low-Mid': ' ', 'low-Right': ' '}
+
+printBoard(theBoard)
+
+playing = True
+putMeOnBoard = 'X'
+# start with 6 moves so
+count = 0
+
+while playing:
+    print("Time to place an " + putMeOnBoard + " on the board")
+    placeChar(theBoard, putMeOnBoard)
+    printBoard(theBoard)
+    if putMeOnBoard == 'X':
+        putMeOnBoard = 'O'
+    else:
+        putMeOnBoard = 'X'
+
+    count = count + 1
+    if count == 6:
+        playing = False
