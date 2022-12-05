e137d5e4f5b9f04f63b8fab616ef0248fee9a3cc
Noah Holt <70286103+TidalTurtles@users.noreply.github.com>
2022-12-05 11:02:13 -0700

    done with part 1
    Done with parts 3 and 1 only 2 left now

--- hw7_noah_holt/hw7_noah_holt_ex_1.py
+++ hw7_noah_holt/hw7_noah_holt_ex_1.py
@@ -18,3 +18,19 @@ Exercise 1: Rename Files with Dates
         loop over all and find American
         to rename call shutil.move()
 '''
+
+import re
+import os
+import shutil
+
+americanDateRegex = re.compile(r'(.*)([01][0-9])-([0123][0-9])-([012][0-9][0-9][0-9])(.*)')
+
+fileList = os.listdir('.')
+
+for file in fileList:
+    datePresent = americanDateRegex.search(file)
+    if datePresent is not None:
+        asianDate = '{}{}-{}-{}{}'.format(datePresent.group(1), datePresent.group(4), datePresent.group(2), datePresent.group(3), datePresent.group(5))
+        shutil.copy(file, asianDate)
+
+
